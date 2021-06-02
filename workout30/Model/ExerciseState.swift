b7dce705a25724ffac4d69aaeb929582959b7ee2
Didier <didier.fuentes@gmail.com>
2021-06-02 14:03:43 -0400

    ExerciseState model.

---
+++ workout30/Model/ExerciseState.swift
@@ -0,0 +1,14 @@
+//
+//  ExerciseState.swift
+//  workout30
+//
+//  Created by Didier on 2/6/21.
+//
+
+import Foundation
+
+enum ExerciseState {
+    case NotStarted
+    case Running
+    case Ended
+}
