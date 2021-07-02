22713a12ad2429f5a5d074b5ec9d3010ce536994
Didier <didier.fuentes@gmail.com>
2021-07-02 14:14:56 -0400

    Shows image when running.

--- workout30/View/ExerciseCell.swift
+++ workout30/View/ExerciseCell.swift
@@ -9,26 +9,27 @@ import UIKit
 
 class ExerciseCell: UITableViewCell {
     static let identifier = String(describing: ExerciseCell.self)
-    lazy var timer: Timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-        self.time -= 1
+    lazy var timer: Timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
+        self?.time -= 1
     })
-
+    let animationTime = 0.2
     var state: ExerciseState = .NotStarted {
         didSet {
             timer.invalidate()
             switch state {
             case .NotStarted:
-                break
+                imageHeight.constant = 0
             case .Running:
                 setTimer()
+                imageHeight.constant = 100
                 timer.fire()
             case .Resting:
                 label.text = L10n.Exercise.rest
-                
+
                 setTimer()
                 timer.fire()
             case .Ended:
-                break
+                imageHeight.constant = 0
             }
         }
     }
@@ -81,8 +82,12 @@ class ExerciseCell: UITableViewCell {
     lazy var image: UIImageView = {
         let i = UIImageView()
         i.translatesAutoresizingMaskIntoConstraints = false
-        i.backgroundColor = .blue
+        i.backgroundColor = .clear
         return i
+    }()
+
+    private lazy var imageHeight: NSLayoutConstraint = {
+        image.heightAnchor.constraint(equalToConstant: 0)
     }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -96,23 +101,25 @@ class ExerciseCell: UITableViewCell {
     }
 
     private func setupView() {
+        backgroundColor = .clear
         contentView.addSubviews([label, _time, image, divider])
         NSLayoutConstraint.activate([
-            image.topAnchor.constraint(equalTo: contentView.topAnchor),
-            image.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
-            image.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16),
-            image.heightAnchor.constraint(equalToConstant: 200),
+            image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            image.widthAnchor.constraint(equalToConstant: 200),
+            image.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageHeight,
 
-            label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 24),
+            label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8),
             label.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
             label.rightAnchor.constraint(equalTo: contentView.centerXAnchor, constant: -8),
-            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            label.bottomAnchor.constraint(equalTo: divider.bottomAnchor, constant: -8),
 
             _time.centerYAnchor.constraint(equalTo: label.centerYAnchor),
             _time.leftAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 8),
             _time.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16),
 
             divider.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            divider.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             divider.heightAnchor.constraint(equalToConstant: 1),
             divider.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
