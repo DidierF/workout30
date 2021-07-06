aaf6326d35f8b4ec946f97f9165067590f3388a2
Didier <didier.fuentes@gmail.com>
2021-07-06 18:32:38 -0400

    Main exercise view.

---
+++ workout30/View/ExerciseView.swift
@@ -0,0 +1,57 @@
+//
+//  ExerciseView.swift
+//  workout30
+//
+//  Created by Didier on 6/7/21.
+//
+
+import UIKit
+
+class ExerciseView: UIView {
+
+    lazy var image: UIImageView = {
+        let i = UIImageView()
+        i.translatesAutoresizingMaskIntoConstraints = false
+        i.clipsToBounds = true
+        return i
+    }()
+
+    lazy var container: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.clipsToBounds = true
+        return v
+    }()
+
+    init(radius: CGFloat) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.shadowColor = CGColor(gray: 0, alpha: 1)
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.cornerRadius = radius
+        clipsToBounds = false
+
+        container.layer.cornerRadius = radius
+        container.addSubview(image)
+
+        addSubview(container)
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: topAnchor),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+            container.leftAnchor.constraint(equalTo: leftAnchor),
+            container.rightAnchor.constraint(equalTo: rightAnchor),
+
+            image.topAnchor.constraint(equalTo: container.topAnchor),
+            image.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+            image.leftAnchor.constraint(equalTo: container.leftAnchor),
+            image.rightAnchor.constraint(equalTo: container.rightAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
